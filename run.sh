da537a542c5920edc7f9bf1d128c7a42065019c1
Robin van der Vleuten <robinvdvleuten@gmail.com>
2014-07-30 10:35:07 +0200

    Clear the Cloudflare cache through a CURL request.

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+response=`curl -s https://www.cloudflare.com/api_json.html \
+  -d "a=fpurge_ts" \
+  -d "tkn=$WERCKER_CLOUDFLARE_PURGE_API" \
+  -d "email=$WERCKER_CLOUDFLARE_PURGE_EMAIL" \
+  -d "z=$WERCKER_CLOUDFLARE_PURGE_DOMAIN" \
+  -d "v=1"`
+
+result=`echo $response | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w result`
+
+if [ ${result##*|} == 'error' ]; then
+  fail "An error occured while purging Cloudflare cache"
+else
+  debug "Successfully purged the Cloudflare cache for domain $WERCKER_CLOUDFLARE_PURGE_DOMAIN"
+fi
